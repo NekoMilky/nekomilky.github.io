1b6c7d9e377fc7e1ee17cca211bb4be9f91778ce
NekoMilky <132798734+NekoMilky@users.noreply.github.com>
2023-06-28 14:57:29 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -12,11 +12,7 @@ function toggledaynight(){
         document.getElementById("mainbox").style.color="rgb(0,0,0)";
         document.getElementById("mainbox").style.backgroundColor="rgba(255,255,255,0.5)";
         document.getElementById("mainbox").style.boxShadow="0 5px 15px rgba(255,255,255,0.5)";
-        document.getElementById("home").style.filter="invert(0)";
-        document.getElementById("wechat").style.filter="invert(0)";
-        document.getElementById("qq").style.filter="invert(0)";
-        document.getElementById("mail").style.filter="invert(0)";
-        document.getElementById("bilibili").style.filter="invert(0)";
+        document.getElementById("contact").style.filter="invert(0)";
     }else{
         document.getElementById("mainpic").style.backgroundImage="url(source/img/background/night.png)";
         document.getElementById("hr").style.border="1px dashed rgb(255,255,255)";
@@ -24,11 +20,7 @@ function toggledaynight(){
         document.getElementById("mainbox").style.color="rgb(255,255,255)";
         document.getElementById("mainbox").style.backgroundColor="rgba(0,0,0,0.5)";
         document.getElementById("mainbox").style.boxShadow="0 5px 15px rgba(0,0,0,0.5)";
-        document.getElementById("home").style.filter="invert(1)";
-        document.getElementById("wechat").style.filter="invert(1)";
-        document.getElementById("qq").style.filter="invert(1)";
-        document.getElementById("mail").style.filter="invert(1)";
-        document.getElementById("bilibili").style.filter="invert(1)";
+        document.getElementById("contact").style.filter="invert(1)";
     }
     return false;
 }
@@ -40,6 +32,7 @@ function toggledaynightmobile(){
         document.getElementById("mainbox-m").style.color="rgb(0,0,0)";
         document.getElementById("mainbox-m").style.backgroundColor="rgba(255,255,255,0.5)";
         document.getElementById("mainbox-m").style.boxShadow="0 5px 15px rgba(255,255,255,0.5)";
+        document.getElementById("contact-m").style.filter="invert(0)";
     }else{
         document.getElementById("mainpic-m").style.backgroundImage="url(source/img/background/nightmobile.png)";
         document.getElementById("hr-m").style.border="1px dashed rgb(255,255,255)";
@@ -47,6 +40,7 @@ function toggledaynightmobile(){
         document.getElementById("mainbox-m").style.color="rgb(255,255,255)";
         document.getElementById("mainbox-m").style.backgroundColor="rgba(0,0,0,0.5)";
         document.getElementById("mainbox-m").style.boxShadow="0 5px 15px rgba(0,0,0,0.5)";
+        document.getElementById("contact-m").style.filter="invert(1)";
     }
     return false;
 }
